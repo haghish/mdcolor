e540536604272edb861cee306365a143843b6b69
E. F. Haghish <haghish.e.f@gmail.com>
2017-10-23 10:57:08 +0200

    Initial commit

---
+++ R/mdcolor.R
@@ -0,0 +1,8 @@
+#' @title Font and Text background Color in Markdown
+#' @description Extending the Markdown markup language with syntax for changing
+#'              the font color and the text background color in the document.
+#' @export
+
+mdcolor <- function() {
+    #cat the script file
+}
