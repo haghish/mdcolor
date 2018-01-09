6efb144d6c75bb4f96a87005df2d8d4c595bccc8
E. F. Haghish <haghish.e.f@gmail.com>
2018-01-09 10:49:18 +0100

    0.3.2

--- R/mdcolor.R
+++ R/mdcolor.R
@@ -1,14 +1,11 @@
 #' All of R colors are supported by \code{mdcolor} package.
 #' you can see the name of R colors by typing \code{color()}
-#' function.
+#' function. the syntax for applying a color to text or
+#' text background are shown below:
 #'
-#' \cr \strong{Changing Text Color and Background Color} \cr
-#' to change text color, use the following format: \cr
 #'
-#' \code{    [colorname] ... [#]} \cr
-#'
-#' to change text background color, use the following format: \cr
-#' \code{    [-colorname] ... [#]} \cr
+#' \code{    [colorname]} change text color \code{[#]} \cr
+#' \code{    [-colorname]} highlight text \code{[#]} \cr
 #'
 #'
 #' as noted above, the syntax is only different in a dash that
@@ -25,6 +22,31 @@
 #'              explanation, see the details section below.
 #' @return HTML code for sourcing the \code{mdcolor.js}
 #'         JavaScript library in the Rmarkdown file
+#'
+#'@examples
+#'\dontrun{
+#' # include the following code chunks in an Rmd file
+#' # ================================================
+#'
+#' ```{r, echo=FALSE, results='asis'}
+#' library(mdcolor)
+#' mdcolor()
+#' ```
+#'
+#' [green]Fun Begins![#]
+#' ---------------------
+#'
+#' This is an R [-yellow] Markdown document[#].
+#' [-purple]Markdown[#] is a simple formatting syntax for
+#' authoring [green]HTML[#], [blue]PDF[#], and [red]MS[#]
+#' Word documents. As you can see, All [-hotpink]R[#]
+#' [orangered]colors[#]are
+#' [-greenyellow][orange]**AVAILABL**[#][#] and the syntax
+#' can be used with Markdown.
+#'
+#' **GitHub Repository**:  https://github.com/haghish/mdcolor
+#'}
+#'
 #' @export
 
 mdcolor <- function() {
